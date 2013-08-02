90ea46a91c6ac4680569e1a9c3f6918ec5d0f229
ghuger <ghuger@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-02 22:15:47 +0000

    Changing stable to 1.1 (bug fix for [after] shortcode)
    git-svn-id: https://plugins.svn.wordpress.org/before-and-after@750308 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.1/trunk/before-and-after.php
@@ -0,0 +1,207 @@
+<?php
+/*
+Plugin Name: Before And After - Lead Capture Plugin for Wordpress
+Plugin URI: http://illuminatikarate.com/before-and-after-plugin/
+Description: Before And After is a lead capture plugin for Wordpress. It allows a webmaster to require visitors to complete a goal, such as filling out a contact form, before viewing the content inside the shortcode. This functionality is also useful when webmaster's want to ensure visitors read a Terms Of Service or Copyright Notice before viewing a given page.
+Author: Illuminati Karate, Inc.
+Version: 1.0
+Author URI: http://illuminatikarate.com
+
+This plugin is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This plugin is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this plugin .  If not, see <http://www.gnu.org/licenses/>.
+*/
+	
+include('include/backfill_functions.php');
+
+
+class BeforeAndAfterPlugin
+{
+
+	var $nextGoalName = false;
+
+	/* Plugin init. Registers shortcodes and starts the session if needed */
+	function __construct()
+	{
+		if(session_id() == '') {
+			session_start();
+		}
+		$this->registerShortcodes();
+	}
+		
+	/* Creates the [goal], [before], [after], and [complete_goal] shortcodes */
+	function registerShortcodes()
+	{
+		add_shortcode('goal', array($this, 'goal_shortcode'));
+		add_shortcode('before', array($this, 'before_shortcode'));
+		add_shortcode('after', array($this, 'after_shortcode'));
+		add_shortcode('complete_goal', array($this, 'complete_goal_shortcode'));
+	}
+	
+	/* Renders the [goal] shortcode
+	 *
+	 * First checks if the specified goal has been completed.
+	 * - If it has, the nested [before] shortcode is rendered and returned. 
+	 * - If it has not, the nested [after] shortcode is rendered and returned. 
+	 * 
+	 * If neither a before nor an after shortcode is contained within the [goal] shortcode,
+	 * the shortcode implicitly assumes its contents should be shown *before* the 
+	 * goal is completed, and nothing is shown after
+	 */	
+	function goal_shortcode($atts, $content)
+	{
+		if (isset($atts['name'])) {
+			$goalName = $atts['name'];
+		} else {		
+			return ''; // name is required. return empty string.
+		}
+		$hasBefore = has_shortcode($content, 'before');
+		$hasAfter = has_shortcode($content, 'after');
+				
+		if (!$hasBefore && !$hasAfter)
+		{
+			$content = '[before]' . $content . '[/before]';
+			$hasBefore = true;
+		}
+
+		if ($this->wasGoalCompleted($goalName))
+		{
+			$shortcodeContent = $hasAfter ? $this->extract_shortcode('after', $content) : 'x';
+			$this->nextGoalName = $goalName;
+			return do_shortcode($shortcodeContent);
+		}
+		else
+		{
+			$shortcodeContent = $hasBefore ? $this->extract_shortcode('before', $content) : 'x';
+			$this->nextGoalName = $goalName;			
+			return do_shortcode($shortcodeContent);
+		}
+	}
+	
+	/* Finds a the given $shortcode inside $content and return it, along with its contemts.
+	 * Returns an empty string if $shortcode is not found within $content
+	 */
+	function extract_shortcode($shortcode, $content)
+	{
+		// first verify that $shortcode is inside $content, before we start in with the regular expressions to extract it
+		if (!has_shortcode($content, $shortcode)) {
+			return '';
+		}
+		// extraction tim!
+		$pattern = get_shortcode_regex(); // this is a wp function, it returns a regex to match all shortcodes which are currently registered
+
+		if (   preg_match_all( '/'. $pattern .'/s', $content, $matches )
+			&& array_key_exists( 2, $matches )
+			&& in_array( $shortcode, $matches[2] ) )
+		{
+			// shortcode is being used somewhere in this arrey (but the array could also contain other shortcodes, because of the regex we used). 
+			// so look through the array and find it!
+			
+			// first step is to find the key's index, from $matches[2]
+			$foundIndex = -1;
+			foreach ($matches[2] as $index => $match) {			
+				if ($match == $shortcode) {
+					$foundIndex = $index;
+					break;
+				}
+			}
+			
+			// if we found the key's index, look for the corresponding entry in $matches[0]
+			// that will contain the entire shortcode, which is what we want to extract!
+			if ($foundIndex >= 0 && isset($matches[0][$foundIndex])) {
+				return $matches[0][$foundIndex];
+			} else {
+				return '';
+			}
+			
+		} else {
+			return '';
+		}
+	
+	}
+	
+	/* Sort of a "mega trim" function that removes <br /> tags from the start and end of a string, along with a normal trim () for whitespace.
+	 * Used to extract nested shortcodes without leaving a bunch of extra whitespace
+	*/
+	function trim_brs($str)
+	{
+		$str = preg_replace('/(<br \/>)+$/', '', $str);
+		$str = preg_replace('/^(<br \/>)*/', '', $str);
+		$str = rtrim(trim($str), '<br />');  	
+		return trim($str);
+	}
+	
+	/* Holds the content which should be shown to a visitor BEFORE they complete a specified goal.
+	 *
+	 * This shortcode expects to be nested inside a [goal] shortcode, and possibly accompanied by an [after] shortcode.
+	 */
+	function before_shortcode($atts, $content)
+	{
+		if ( $this->nextGoalName != '' && $this->wasGoalCompleted($this->nextGoalName) ){
+			return '';
+		}
+		else {
+			$trimmedContent = $this->trim_brs($content);
+			return do_shortcode($trimmedContent);
+		}		
+	}
+	
+	/* Holds the content which should be shown to a visitor AFTER they complete a specified goal.
+	 *
+	 * This shortcode expects to be nested inside a [goal] shortcode, and possibly accompanied by a [before] shortcode.
+	 */
+	function after_shortcode($atts, $content)
+	{
+		if ( $this->nextGoalName != '' && $this->wasGoalCompleted($this->nextGoalName) ){
+			$trimmedContent = $this->trim_brs($content);
+			return do_shortcode($trimmedContent);
+		}
+		else {
+			return '';
+		}		
+	}
+
+	/* [complete_goal] shortcode
+	 *
+	 * This shortcode should be included on a page, e.e., a Thank You page after a newsletter signup form, 
+	 * and marks the current visitor as having completed the goal (via a $_SESSION variable).
+	 */
+	function complete_goal_shortcode($atts, $content)
+	{
+		if (isset($atts['name'])) {
+			$goalName = $atts['name'];
+			return $this->completeGoal($goalName);
+		}
+		return '';
+	}
+	
+	/* Place a session variable that marks the current visitor as having completed the specified goal */
+	function completeGoal($goalName)
+	{
+		$sessionKey = 'goal_' . md5($goalName);
+		$sessionValue = 'goal_completed_' . md5($goalName);
+		$_SESSION[$sessionKey] = $sessionValue;
+		return '';
+	}
+	
+	/* Returns true/false, indicated whether the specified goal has been completed */
+	function wasGoalCompleted($goalName)
+	{
+		$sessionKey = 'goal_' . md5($goalName);
+		$sessionValue = 'goal_completed_' . md5($goalName);
+		return (isset($_SESSION[$sessionKey]) && $_SESSION[$sessionKey] == $sessionValue);
+	}
+	
+}
+
+// Instantiate one copy of the plugin class, to kick things off
+$beforeAndAfter = new BeforeAndAfterPlugin();
---
+++ tags/1.1/trunk/readme.txt
@@ -0,0 +1,101 @@
+﻿=== Before And After: Lead Capture Plugin For Wordpress ===
+Contributors: ghuger
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=V7HR8DP4EJSYN
+Tags: lead capture, lead capture form, lead capture plugin, protected content, gated content, click wrap, click wrapper, tos wrap, tos wrapper, copyright notice, copyright wrapper
+Requires at least: 3.0.1
+Tested up to: 3.5.2
+Stable tag: 1.1
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Before And After is a lead capture plugin for Wordpress. Use it to require visitors to complete a goal, i.e., filling out a form, before continuing. 
+
+== Description ==
+
+Before And After is a lead capture plugin for Wordpress. It allows a webmaster to require visitors to complete a goal, such as filling out a contact form, before viewing the contents of a page. 
+
+This functionality is also useful when webmaster's want to ensure visitors read a Terms Of Service, Copyright Notice, or other important message before viewing a given page or bit of content.
+
+The secret sauce of Before And After is encapsulated by 4 new shortcodes: [goal], [before], [after], and [completed_goal]. These shortcodes are intended to be used together to display dynamic content based on whether the visitor has completed a goal.
+
+Below is an example of a classic lead capture form. We're asking the visitor to complete the lead capture form before they are granted access to the whitepaper.
+
+Here's how to capture a lead for your whitepaper with the Before And After plugin:
+
+### Step 1: On The Landing Page, Add The [goal]. [before], and [after] shortcodes
+
+<code>
+	[goal name="Completed Contact Form"]
+		[before]
+		Please complete our contact form for access to this whitepaper.
+
+		<contact form>
+		[/before]
+		[after]
+		Thank you for completing our contact form! Please find your whitepaper here: http://example.com/whitepaper.pdf
+		{/after]
+	[/goal]
+</code>
+
+Note: Replace <contact form> with your form of choice. That can be a Wordpress plugin like Contact Form 7 or Gravity Forms, or a 3rd party form such as Mailchimp or Aweber mailing list signup forms.
+
+### Step 2: On The Thank You Page, Add The [completed_goal] Shortcode
+
+<code>
+	[completed_goal name="Completed Contact Form"]
+</code>
+
+Note: The Thank You page is the page to which the visitor is sent after completing the contact form. You'll need to configure your form to send the visitor to the URL you've chosen for the Thank You page.
+
+That's all! This is all that is required to setup a basic lead capture form with Wordpress and the Before And After Lead Capture plugin. 
+
+Now that we've setup our pages and shortcodes, let's dive deeper into what will happen from the user's perspective:
+
+When the user first encounters the landing page, they will not have completed our goal (which is identified as "Completed Contact Form"). Until they complete this goal (by filling out the form), they will always see the content inside the [before] shortcode. 
+
+Keep in mind - the content inside the [before] shortcode can be anything: a Contact Form 7 form, a welcome video, a Terms Of Service agreement - any content that Wordpress supports can be placed inside the [before] or [after] shortcode. In our example above, we've added a short message and a contact form inside the [before] shortcode.
+
+Next, the user completes the contact form and is sent to the Thank You page, where they encounter the [completed_goal name="Completed Contact Form"] shortcode that we added in Step 2. This marks them as having completed the "Completed Contact Form" goal.
+
+Now, when this visitor encounters another [goal name="Completed Contact Form"] shortcode, they'll see the content inside the [after] shortcode instead. This can be a link to the whitepaper, a plot spoiler, or any other content that you'd like to reveal to the visitor.
+
+Using these simple shortcodes, any number of scenarios are possible:
+
+ - Lead Capture Forms: Ask a visitor to signup for your newsletter in return for a free download, special report, or whitepaper
+ - Terms Of Service Pages: Make sure a visitor reads the terms of service first. Once they have read the TOS once, they may view any other page.
+ - Age Gate - Make the visitor confirm their age before browsing a given page. 
+ - Copyright Notice: Inform visitors of the copyright of a particular piece of content before allowing them to view it.
+ - Instructions In Series: Make sure that a visitor reads a series of instructions in sequence. If they land on a later page, ask them to start over.
+ - Guided Product Tours: Show your users the screens of your product in a sequenced progression
+
+There are many other possibilities. By offering Wordpress webmasters a simple way to gate content we hope to provide a useful tool for many scenarios.
+
+More Information Available Here: https://illuminatikarate.com/before-and-after-plugin/
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Upload the contents of `/before-and-after/` to the `/wp-content/plugins/` directory
+2. Activate the Before And After Lead Capture Plugin through the 'Plugins' menu in WordPress
+3. Visit this address for information on how to configure the plugin: https://illuminatikarate.com/before-and-after-plugin/
+
+== Frequently Asked Questions ==
+
+= Where Is The Settings Page? =
+
+There isn't one (yet)! Before And After's functionality is expressed through its four shourtcodes: [before],[after],[goal], and [goal_complete]
+
+= How Do I Create A New Goal? =
+
+Just change the value in the name attribute of your shortcode to anything you want. As long as you use the same name attribute in the [goal] and [goal_complete] shortcodes everything will work together as you want.
+
+= How Do I Reset My Form, So That Everyone Has To Complete It Again? =
+
+Just change the name attribute to something else. Perhaps add a "2" at the end. Just make sure you update the [goal] and the [goal_complete] shortcode so they match.
+
+= 1.1 =
+* Fixing a bug with the [after] shortcode
+
+= 1.0 =
+* Initial Release!
